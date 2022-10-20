e62e74836bf7f8e55ca907fa1bd9ab53ef6ea627
Avinash R <112320598+Avinash-R-29@users.noreply.github.com>
2022-10-20 20:32:32 +0530

    Create TestTwoArrays.java
    Java program to test the equality of two arrays

---
+++ TestTwoArrays.java
@@ -0,0 +1,14 @@
+public class TestTwoArrays {
+  public static void  main(String[] args)
+  {
+  int[] array1 =  {2, 5, 7, 9, 11};
+  int[] array2 =  {2, 5, 7, 8, 11};
+  if (array1 == array2)
+        {
+       System.out.println("Two arrays are equal.");
+        }
+  else
+  {
+      System.out.println("Two  arrays are not equal.");
+       }
+  }
